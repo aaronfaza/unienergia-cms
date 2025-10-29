e923eef33e296ed61b49fee00d1cf108c9bcdd40
Aaron Joel Farias Zavala <aaron.farias@unienergia.pe>
2025-10-29 13:57:28 -0500

    camvbios

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+module.exports = ({ env }) => ({
+  host: '0.0.0.0',  // Permite conexiones externas
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
+  url: 'https://panel.unienergia.pe', // Tu dominio
+  proxy: true,
 });
+
